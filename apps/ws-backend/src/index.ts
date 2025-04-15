5d73ba3698acbaa331da0da7b78474a1e50d6eb2
Abhinav Chauhan <abhinav@Abhinavs-MacBook-Air.local>
2025-04-16 04:23:10 +0530

    added authentication in ws server

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,25 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload, verify } from "jsonwebtoken";
 const wss = new WebSocketServer({ port: 8080 });
+import dotenv from "dotenv";
+dotenv.config();
 
-wss.on("connection", function connection(ws) {
+wss.on("connection", function connection(ws, request) {
   console.log("new connection ");
+
+  const url = request.url;
+  if (!url) {
+    return;
+  }
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token") || "";
+
+  const decoded = verify(token, process.env.jwtSecret as string);
+  if (!decoded || !(decoded as JwtPayload).userId) {
+    ws.close();
+    return;
+  }
+
   ws.on("message", function message(data) {
     ws.send("pong");
   });
